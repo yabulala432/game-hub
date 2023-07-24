44e75556d1cea9084048f0c5cd41de5e46d38412
Yeabsira Yonas <yabulala432@gmail.com>
2023-07-24 20:51:36 +0300

    install chacra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import './App.css'
+import { Button } from '@chakra-ui/react'
 
-function App() {}
+function App() {
+  return (
+    <>
+<Button colorScheme='blue'>Button</Button>
+      </>
+  )
+}
 
 export default App

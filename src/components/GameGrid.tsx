41d773b752e358587855cb6cf7ce7cdc6cea3165
Yeabsira Yonas <yabulala432@gmail.com>
2023-07-25 18:58:30 +0300

    Refactor: useGame

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,7 +5,7 @@ import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 
 export default function GameGrid() {
-  const { games, error, loading } = useGames();
+  const { data, error, loading } = useGames();
   const numberofSkeletons = [1, 2, 3, 4, 5, 6, 7, 8];
   return (
     <>
@@ -21,7 +21,7 @@ export default function GameGrid() {
               <GameCardSkeleton />
             </GameCardContainer>
           ))}
-        {games.map((game) => {
+        {data.map((game) => {
           return (
             <GameCardContainer>
               <GameCard key={game.id} game={game} />{" "}
